02bfab1d9851087ef071c9ae79e26e34a3ebe65c
saurang1984 <saurang1984@gmail.com>
2024-10-24 13:38:47 -0500

    Update Program.cs

--- ProjectTwo/Program.cs
+++ ProjectTwo/Program.cs
@@ -12,6 +12,10 @@
             int vowelCount = processor.CountVowels(inputString);
             Console.WriteLine($"The number of vowels in '{inputString}' is: {vowelCount}");
 
+            string inputString = "hello Saurang";
+            int vowelCount = processor.CountVowels(inputString);
+            Console.WriteLine($"The number of vowels in '{inputString}' is: {vowelCount}");
+
             // Sample usage of the Reverse method
             string reversedString = processor.Reverse(inputString);
             Console.WriteLine($"The reverse of '{inputString}' is: '{reversedString}'");
